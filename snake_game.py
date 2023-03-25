cd1d96aa6518e3db59c47ac483152a5997ca1a2f
fola789 <49055102+fola789@users.noreply.github.com>
2023-03-25 21:28:13 +0000

    Add display to snake game
    Added display to snake game

--- snake_game.py
+++ snake_game.py
@@ -4,26 +4,31 @@ import random
 
 pygame.init()
 
-Class SnakeGame:
+class SnakeGame:
 
     def __init__(self, w=640, h=480):
         self.w = w
         self.h = h
         # init display
-            
+        self.display = pygame.display.set_mode((self.w, self.h))
+        pygame.display.set_caption('Snake')
+        self.clock = pygame.time.Clock()
         # init game state
+        
+        
+        
     def play_step(self):
         pass
         
 if __name__ == '__main__':
-        game = SnakeGame()
+    game = SnakeGame()
         
-        # game loop
-        while True:
-            game.play_step()
+    # game loop
+    while True:
+        game.play_step()
             
-            # break if game over
+        # break if game over
             
-        pygame.quit()
+    pygame.quit()
     
     
