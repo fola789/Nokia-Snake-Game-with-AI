52ebbb0d95d867fd95c17c2a2165d18b65150e89
fola789 <49055102+fola789@users.noreply.github.com>
2023-03-25 21:19:43 +0000

    Update snake_game.py

--- snake_game.py
+++ snake_game.py
@@ -1,43 +1,29 @@
 import pygame
 import random
-from enum import Enum
-from collections import namedtuple
+
 
 pygame.init()
-font  = pygame.font.Font('arial.ttf',25)
 
-class Direction(Enum):
-        RIGHT = 1
-        LEFT = 2
-        UP = 3
-        DOWN = 4
-        
-Point - namedtuple('Point', 'x, y')
-   
-# rgb colors
-WHITE = (255, 255, 255)
-RED = (200,0,0)
-BLUE1 = (0, 0, 255)
-BLUE2 = (0, 100, 255)
-BLACK = (0,0,0)
+Class SnakeGame:
 
-BLOCK_SIZE = 20
-SPEED = 20
-
-class SnakeGame:
-    
     def __init__(self, w=640, h=480):
         self.w = w
         self.h = h
         # init display
-        self.display = pygame.display.set_mode((self.w, self.h))
-        pygame.display.set_caption('Snake')
-        self.clock = pygame.time.Clock()
-        
+            
         # init game state
-        self.direction = Direction.RIGHT
+    def play_step(self):
+        pass
         
-        self.head = Point(self.w/2, self.h/2)
-        self.snake = [self.head, 
-                      Point(self.head.x-BLOCK_SIZE, self.head.y),
-                      Point(self.head.x-(2*BLOCK_SIZE), self.head.y)]
+if __name__ == '__main__':
+        game = SnakeGame()
+        
+        # game loop
+        while True:
+            game.play_step()
+            
+            # break if game over
+            
+        pygame.quit()
+    
+    
