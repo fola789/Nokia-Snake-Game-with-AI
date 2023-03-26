49f68193c8cf7963eb8f7fd07595cd7e0e872c8e
fola789 <49055102+fola789@users.noreply.github.com>
2023-03-26 22:58:45 +0100

    Update snake_game.py
    More changes and stuff

--- snake_game.py
+++ snake_game.py
@@ -1,9 +1,19 @@
 import pygame
 import random
-
+from enum import Enum
+from collections import namedtuple
 
 pygame.init()
 
+class Direction(Enum):
+    RIGHT = 1
+    LEFT = 2 
+    UP  = 3
+    DOWN = 4
+
+Point = namedtuple('Point', 'x', 'y')
+
+BLOCK_SIZE = 20
 class SnakeGame:
 
     def __init__(self, w=640, h=480):
@@ -11,11 +21,24 @@ class SnakeGame:
         self.h = h
         # init display
         self.display = pygame.display.set_mode((self.w, self.h))
-        pygame.display.set_caption('Snake')
+        pygame.display.set_caption('Snake') 
         self.clock = pygame.time.Clock()
+        
         # init game state
+        self.direction = Direction.RIGHT
         
+        self.head = Point(self.w/2, self.h/2)
+        self.snake = [self.head, 
+                      Point(self.head.x-BLOCK_SIZE, self.head.y),
+                      Point(self.head.x-(2*BLOCK_SIZE, self.head.y)]
         
+        self.score = 0
+        self.food = None
+        self._place_food()
+        
+    def _place_food(self):
+        x = random.randomint(0, (self.w-BLOCK_SIZE)//BLOCK_SIZE )*BLOCK_SIZE
+        y = random.randomint(0, (self.h-BLOCK_SIZE)//BLOCK_SIZE )*BLOCK_SIZE
         
     def play_step(self):
         pass
